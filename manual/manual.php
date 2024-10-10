10f15621f0e11ad890dfd80ef359eb50cb6ed9ad
Joao-php404 <jvlopessantos58@gmail.com>
2024-10-09 21:16:55 -0300

    Update manual.php

--- manual/manual.php
+++ manual/manual.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="ajuda.css">
     <title>Fiscal Cidadão - Manual de Usuário</title>
 </head>
 <body>
